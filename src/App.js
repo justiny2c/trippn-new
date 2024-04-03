577f6afef1907a1c68dc4df928b3bedf666e4523
Justin Chen <justiny2c@gmail.com>
2024-04-03 13:11:46 -0700

    Added AboutUs page. Still working on bottom half styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './HomePage';
 import PlanTripPage from './PlanTripPage';
 import NavBar from './NavBar';
 import ItineraryPage from './ItineraryPage';
+import AboutUs from './AboutUs';
 import ChatGPT from './ChatGPT';
 import './App.css';
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" element={<HomePage />} />
           <Route path="/plan-trip" element={<PlanTripPage />} />
           <Route path="/itinerary" element={<ItineraryPage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/chat" element={<ChatGPT />} />
         </Routes>
     </div>
