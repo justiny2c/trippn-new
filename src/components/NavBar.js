b4f8cf31a51e1eab14cd3ea97519a29405ca73de
Justin Chen <justiny2c@gmail.com>
2024-04-07 22:42:31 -0700

    Added Hamburger menu for NavBar. Styling may need a little work, but is functional for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoBlue from "../images/trippn-blue-sm.png"
 import {NavLink} from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
+
+  const [isActive, setIsActive] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
         <nav className="navbar">
           <NavLink to="/">
             <img src={LogoBlue} className="logo" alt="Trippn Logo"/>
           </NavLink>
-          <div className="nav-links">
-            <a href="/plan-trip">Plan A Trip</a>
-            <a href="/trips">Itineraries</a>
-            <a href="/about-us">About Us</a>
-            {/* <a href="/faq">FAQ</a> */}
-            <a href="/account">Account</a>
-          </div>
+            <div className={`hamburger ${isActive ? 'active' : ''}`} onClick={() => setIsActive(!isActive)}>
+              <div className={`bar1 ${isActive ? 'change' : ''}`}></div>
+              <div className={`bar2 ${isActive ? 'change' : ''}`}></div>
+              <div className={`bar3 ${isActive ? 'change' : ''}`}></div>
+            </div>
+
+            <div className={`nav-links ${isActive ? 'active' : ''}`}>
+              <a href="/plan-trip">Plan A Trip</a>
+              <a href="/trips">Itineraries</a>
+              <a href="/about-us">About Us</a>
+              {/* <a href="/faq">FAQ</a> */}
+              <a href="/account">Account</a>
+            </div>
       </nav>
     </div>
   );
