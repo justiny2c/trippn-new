dd3d9c978a8146bd501dfacf6395f6d1d06b8566
Justin Chen <justiny2c@gmail.com>
2024-04-10 12:17:59 -0700

    Swapped API url from deployed backend

--- src/pages/PlanTripPage.js
+++ src/pages/PlanTripPage.js
@@ -20,7 +20,7 @@ const PlanTripPage = () => {
 
   const handleSubmit = async (e) => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/chat', {
+      const response = await fetch('https://trippn-ai-fd36c0a9cdb0.herokuapp.com/api/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
