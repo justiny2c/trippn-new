6ab732d7228f0cc147132ec828b0a8b9ba717027
Justin Chen <justiny2c@gmail.com>
2024-04-11 13:07:36 -0700

    ResponsePage up. Working on Styling for now

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,12 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import PlanTripPage from './PlanTripPage';
 import NavBar from '../components/NavBar';
-import ItineraryPage from './ItineraryPage';
+// import ItineraryPage from './ItineraryPage';
 import AboutUs from './AboutUs';
-import ChatGPT from './ChatGPT';
-import './App.css';
+import ResponsePage from "./ResponsePage"
 import Footer from '../components/Footer';
-
+import './App.css';
 
 function App() {
   return (
@@ -17,9 +16,9 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/plan-trip" element={<PlanTripPage />} />
-          <Route path="/itinerary" element={<ItineraryPage />} />
+          <Route path="/itinerary" element={<ResponsePage />} />
           <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/chat" element={<ChatGPT />} />
+          {/* <Route path="/chat" element={<ChatGPT />} /> */}
         </Routes>
        <Footer /> 
     </div>
