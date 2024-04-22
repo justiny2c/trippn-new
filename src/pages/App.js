3f8d01203fbb109e7392cd5cd4679ffd059d989e
Justin Chen <justiny2c@gmail.com>
2024-04-22 12:43:28 -0700

    Working on GoogleCalendar, display is working. Month prev and next are working in CalendarHeader. Created a CalendarContext

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ProtectedRoute from '../components/ProtectedRoute';
 import { AuthProvider } from '../contexts/AuthContext';
-import { ItineraryProvider } from '../contexts/ItineraryContext';
+// import { ItineraryProvider } from '../contexts/ItineraryContext';
+import { CalendarProvider } from '../contexts/CalendarContext';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Login from "../components/Login";
@@ -22,22 +23,24 @@ function App() {
   return ( 
     <div>
       <AuthProvider>   
-        <ItineraryProvider>              
-              <NavBar />
-                <Routes>
-                  <Route exact path="/" element={<HomePage />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/" element={<ProtectedRoute />}>
-                      <Route path="plan-trip" element={ <PlanTripPage />} />
-                      <Route path="itinerary" element={<ResponsePage />} />
-                      <Route path="calendar" element={<MyCalendar />} />
-                      <Route path="loading" element={<LoadingScreen />} />
-                  </Route>
-                  <Route path="about-us" element={<AboutUs />} />
-                  <Route path="google" element={<GoogleCalendar />} />
-                </Routes>
-              <Footer />                 
-        </ItineraryProvider>
+        {/* <ItineraryProvider>               */}
+        <CalendarProvider>
+          <NavBar />
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/" element={<ProtectedRoute />}>
+                <Route path="plan-trip" element={ <PlanTripPage />} />
+                <Route path="itinerary" element={<ResponsePage />} />
+                <Route path="calendar" element={<MyCalendar />} />
+                <Route path="loading" element={<LoadingScreen />} />
+              </Route>
+              <Route path="about-us" element={<AboutUs />} />
+                <Route path="google" element={<GoogleCalendar />} />
+            </Routes>
+          <Footer />   
+        </CalendarProvider>              
+        {/* </ItineraryProvider> */}
       </AuthProvider>  
     </div>  
   )
--- src/unused/calendarcomponents/CalendarHeader.js
+++ src/unused/calendarcomponents/CalendarHeader.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import logoPlane from "../../icons/trippn-logo-plane.png"
+import CalendarContext from "../../contexts/CalendarContext"
 
 export default function CalendarHeader() {
-  return (
-    <div>CalendarHeader</div>
-  )
+    const { monthIndex, setMonthIndex } = useContext(CalendarContext)
+    function handlePrevMonth() {
+        setMonthIndex(monthIndex - 1)
+    }
+    function handleNextMonth() {
+        setMonthIndex(monthIndex + 1)
+    }
+
+    return (
+        <header className='px-4 py-2 flex items-center'>
+            <img src={logoPlane} alt="logo" className='mr-2 w-12 h-12' />
+            <h1 className='mr-10 text-xl text-gray-500 font-semibold'>
+                Calendar
+            </h1>
+            <button className='border rounded py-2 px-4 mr-5'>
+                Today
+            </button>
+            <button onClick={handlePrevMonth}>
+                <span className='material-icons-outlined cursor-pointer text-gray-600 mx-2'>
+                    chevron_left
+                </span>
+            </button>
+            <button onClick={handleNextMonth}>
+                <span className='material-icons-outlined cursor-pointer text-gray-600 mx-2'>
+                    chevron_right
+                </span>
+            </button>        
+        </header>
+    )
 }
