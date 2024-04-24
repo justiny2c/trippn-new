d1f8831ea1f4bea8101f16d1827e465db11ab699
Justin Chen <justiny2c@gmail.com>
2024-04-24 12:14:43 -0700

    Selected label works

--- src/unused/calendarcomponents/EventModal.js
+++ src/unused/calendarcomponents/EventModal.js
@@ -1,9 +1,21 @@
 import React, { useContext, useState } from 'react'
 import CalendarContext from '../../contexts/CalendarContext'
 
+const labelsClasses = [
+    "indigo", 
+    "gray", 
+    "green", 
+    "blue", 
+    "red", 
+    "purple"
+]
+
 export default function EventModal() {
     const {setShowEventModal, daySelected} = useContext(CalendarContext)
     const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [selectedLabel, setselectedLabel] = useState(labelsClasses[0])
+
 
     return (
     <div className='h-screen w-full fixed left-0 top-0 flex justify-center items-center'>
@@ -34,6 +46,36 @@ export default function EventModal() {
                         schedule
                     </span>
                     <p>{daySelected.format("dddd, MMMM DD")}</p>
+                    <span className='material-icons-outlined text-gray-400'>
+                        segment
+                    </span>
+                    <input 
+                        type="text" 
+                        name="description" 
+                        placeholder='Add a description' 
+                        value={description} 
+                        required 
+                        className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500" 
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <span className='material-icons-outlined text-gray-400'>
+                        bookmark_border
+                    </span>
+                    <div className='flex gap-x-2'>
+                        {labelsClasses.map((lblClass, i) => (
+                            <span key={i}
+                                onClick={() => setselectedLabel(lblClass)}
+                                className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
+                            >
+                                {selectedLabel === lblClass && (
+                                    <span className='material-icons-outlined text-white text-sm'>
+                                        check
+                                    </span>     
+                                ) }
+                                
+                            </span>
+                        ))}
+                    </div>
                 </div>
             </div>
         </form>
